15aa6d7985b1a8d1376439a7ddf5dbce479cb13e
Allan Jeremy | Doge <aj@dogelabsvr.com>
2026-02-06 19:30:45 +0300

    fix: update database connection to use env variable

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 // TODO: Inject context
 export function createDbConnection(env: any) {
-  // const dbUrl = process.env.DATABASE_URL ?? "";
+  // const dbUrl = env.DATABASE_URL;
   const dbUrl = env.HYPERDRIVE.connectionString;
   console.log("dbUrl: ", dbUrl);
   if (!dbUrl) {
